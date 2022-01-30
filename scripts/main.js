ef70cb8112039f82d3b410ec58bd6e8fe80be060
JDavison92 <davison462901@gmail.com>
2022-01-30 12:17:13 -0600

    Artwork update

--- scripts/main.js
+++ scripts/main.js
@@ -13,13 +13,13 @@ let myHeading = document.querySelector('h1');
 function setUserName() {
     let myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'Artwork by: ' + myName;
+    myHeading.textContent = 'whats up? ' + myName;
   }
   if(!localStorage.getItem('name')) {
     setUserName();
   } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Whats up ' + storedName;
+    myHeading.textContent = 'Whats up? ' + storedName;
   }
   myButton.onclick = function() {
     setUserName();
